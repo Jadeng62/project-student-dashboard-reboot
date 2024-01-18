aa8fe710048ab60d8dcd095bb555c63a2bafe583
Carlitos <carlitosdutan@pursuit.org>
2024-01-17 22:25:02 -0500

    CardListing functionality complete

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./Card.css";
 
-export const Card = () => {
-  return <div>Card</div>;
+export const Card = ({ student }) => {
+  return (
+    <div>
+      <div>
+        <img src={student.profilePhoto} alt="" />
+      </div>
+      <div>
+        <h3>
+          {student.names.preferredName} {student.names.surname}
+        </h3>
+      </div>
+    </div>
+  );
 };
--- src/CardListing.jsx
+++ src/CardListing.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CardListing.css";
+// import { getAllStudents } from "./data/fetch";
+import { Card } from "./Card";
+
+const URL = "http://localhost:5001/api/students";
 
 export const CardListing = () => {
-  return <div>CardListing</div>;
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => setStudents(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      {students.map((student) => {
+        return <Card key={student.id} student={student} />;
+      })}
+    </div>
+  );
 };
