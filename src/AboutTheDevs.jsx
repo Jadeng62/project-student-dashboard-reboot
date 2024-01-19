34e2706d68928659ad390b7cab01d19069965f1e
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-19 17:12:05 -0500

    routing that will be refactored

--- src/AboutTheDevs.jsx
+++ src/AboutTheDevs.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const AboutTheDevs = () => {
-  return <div>AboutTheDevs</div>;
+  return (
+    <div className="bg-blue-500">
+      <Link to={"/"}>Link</Link>
+      <div className="">H</div>
+    </div>
+  );
 };
