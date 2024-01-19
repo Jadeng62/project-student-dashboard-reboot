f41200ceb7aa9858467252e6ff5c87cde155615a
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-18 23:10:11 -0500

    button spacing

--- src/header.jsx
+++ src/header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { MdSchool } from "react-icons/md";
 import { RiTeamFill } from "react-icons/ri";
+import { LuSunMoon } from "react-icons/lu";
 
 import "./Header.css";
 
@@ -16,18 +17,20 @@ export const Header = () => {
             Student Dashboard
           </h1>
         </Link>
-        <span className="flex flex-auto my-2 py-1 justify-end ml-20 pl-10">
+        <span className="flex flex-auto mt-12 mr-14 py-4 pr-1 justify-end ml-20 pl-10 ">
           {/* this theme will become a toggle button that will show sun/moon */}
           <button
             type="button"
-            className=" mx-1 px-2 rounded-full bg-black text-white hover:bg-blue-200"
+            className=" mx-1 mr-6 px-3 py-3 rounded-full bg-black text-white hover:bg-blue-200"
           >
-            Light Dark Toggle
+            {/* Light Dark Toggle */}
+            <LuSunMoon />
           </button>
           <button
             type="button"
-            className=" mx-1 px-2 rounded-full bg-black text-white hover:bg-green-200"
+            className=" mx-1 px-3 py-3 rounded-full bg-black text-white hover:bg-green-200"
           >
+            {/* About the Devs  */}
             <RiTeamFill className="" />
           </button>
         </span>
