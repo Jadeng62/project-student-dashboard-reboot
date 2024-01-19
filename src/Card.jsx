505f636f51fb0d26e68176c3dc7768810b64ea16
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-19 12:58:47 -0500

    revive accomplished

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,34 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import "./Card.css";
-// import { birthdayFormat, graduationTrack } from "./data/helper";
-
-// export const Card = ({ student }) => {
-//   const [track, setTrack] = useState(false);
-
-//   useEffect(() => {
-//     setTrack(graduationTrack(student));
-//   }, [student]);
-
-//   return (
-//     <div className="student-card">
-//       <div>
-//         <img src={student.profilePhoto} alt="no photo provided" />
-//       </div>
-//       <div>
-//         <h3>
-//           {student.names.preferredName} {student.names.middleName}{" "}
-//           {student.names.surname}
-//         </h3>
-//         <p>{student.username}</p>
-//         <p>
-//           <span>Birthday: </span>
-//           {birthdayFormat(student)}
-//         </p>
-//       </div>
-//     </div>
-//   );
-// };
-
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Card.css";
@@ -41,7 +10,6 @@ export const Card = ({ student }) => {
     setTrack(graduationTrack(student));
   }, [student]);
 
-  console.log("student from card", student);
   return (
     <div className="student-card">
       <div>
--- src/CardListing.jsx
+++ src/CardListing.jsx
@@ -5,15 +5,6 @@ import { Card } from "./Card";
 const URL = "http://localhost:5001/api/students";
 
 export const CardListing = ({ selectStudents }) => {
-  // const [students, setStudents] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(URL)
-  //     .then((response) => response.json())
-  //     .then((data) => setStudents(data))
-  //     .catch((error) => console.error("Error fetching data:", error));
-  // }, []);
-
   return (
     <div>
       <h2>All Cohorts</h2>
