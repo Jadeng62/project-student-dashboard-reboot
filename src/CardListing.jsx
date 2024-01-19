9accad93d06e2b4f2b14c0ebb2a075177365119f
jadenglenn <jadenglenn@pursuit.org>
2024-01-19 10:50:48 -0500

    starting new

--- src/CardListing.jsx
+++ src/CardListing.jsx
@@ -4,21 +4,19 @@ import { Card } from "./Card";
 
 const URL = "http://localhost:5001/api/students";
 
-export const CardListing = ({ selectStudents }) => {
-  // const [students, setStudents] = useState([]);
+export const CardListing = () => {
+  const [students, setStudents] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(URL)
-  //     .then((response) => response.json())
-  //     .then((data) => setStudents(data))
-  //     .catch((error) => console.error("Error fetching data:", error));
-  // }, []);
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => setStudents(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
     <div>
-      <h2>All Cohorts</h2>
-      <h2>Total Fellows: {selectStudents.length} Fellows</h2>
-      {selectStudents.map((student) => {
+      {students.map((student) => {
         return <Card key={student.id} student={student} />;
       })}
     </div>
--- src/header.jsx
+++ src/header.jsx
@@ -5,29 +5,15 @@ import "./Header.css";
 
 export const Header = () => {
   return (
-    <div className=" mx-2 my-2">
-      <div className="flex flex-row p-5 rounded-lg bg-red-500">
-        <Link to="/">
-          <h1 className=" text-5xl font-semibold text-white">
-            Student Dashboard
-          </h1>
-        </Link>
-        <span className="flex flex-auto my-2 py-1 justify-end ml-20 pl-10">
-          {/* this theme will become a toggle button that will show sun/moon */}
-          <button
-            type="button"
-            className=" mx-1 px-2 rounded-full bg-black text-white hover:bg-blue-200"
-          >
-            Light Dark Toggle
-          </button>
-          <button
-            type="button"
-            className=" mx-1 px-2 rounded-full bg-black text-white hover:bg-green-200"
-          >
-            About the Devs
-          </button>
-        </span>
-      </div>
-    </div>
+    <nav className="bg-red-500">
+      <Link to="/">
+        <h1 className="text-5xl font-semibold text-white">Student Dashboard</h1>
+      </Link>
+      <span>
+        {/* this theme will become a toggle button that will show sun/moon */}
+        <button type="button">Light Dark Toggle</button>
+        <button>About the Devs</button>
+      </span>
+    </nav>
   );
 };
