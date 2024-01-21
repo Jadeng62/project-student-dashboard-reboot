f659994104f1024f51f29d8b9ba8312f02f0f682
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-20 23:08:57 -0500

    linked footer to app.jsx and added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./Header";
 import { CardListing } from "./CardListing";
 import { Aside } from "./aside";
+import { Footer } from "./footer";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { StudentShow } from "./StudentShow";
 import { AboutTheDevs } from "./AboutTheDevs";
@@ -75,6 +76,7 @@ function App() {
         />
         <Route path="/aboutthedevs" element={<AboutTheDevs />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
