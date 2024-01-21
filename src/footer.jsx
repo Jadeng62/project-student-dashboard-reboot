5d78ec527bce47e4b4bd7c388ca601a7177ec282
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-21 12:22:17 -0500

    properly formatted classes for footer

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,10 +3,10 @@ import { BsGithub } from "react-icons/bs";
 
 export const Footer = () => {
   return (
-    <section class="bg-white">
-      <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-        <nav class="flex flex-wrap justify-center -mx-5 -my-2">
-          <div class="px-5 py-2">
+    <section className="bg-white">
+      <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+        <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+          <div className="px-5 py-2">
             <Link
               to={`/aboutthedevs`}
               className="text-base leading-6 text-gray-500 hover:text-gray-900"
@@ -14,21 +14,21 @@ export const Footer = () => {
               About
             </Link>
           </div>
-          <div class="px-5 py-2">
+          <div className="px-5 py-2">
             <Link
               to="/"
-              class="text-base leading-6 text-gray-500 hover:text-gray-900"
+              className="text-base leading-6 text-gray-500 hover:text-gray-900"
             >
               Home
             </Link>
           </div>
         </nav>
-        <div class="flex justify-center mt-8 space-x-6">
+        <div className="flex justify-center mt-8 space-x-6">
           <Link to="https://github.com/Jadeng62/project-student-dashboard-reboot">
             <BsGithub className="text-gray-400 hover:text-gray-500 text-2xl" />
           </Link>
         </div>
-        <p class="mt-8 text-base leading-6 text-center text-gray-400">
+        <p className="mt-8 text-base leading-6 text-center text-gray-400">
           Copyright © 2024 Herb Matrix Code Rhythm, Inc.
         </p>
       </div>
