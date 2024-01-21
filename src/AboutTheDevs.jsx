a5cc03763f5d8650019551f9aea86774dfdf123a
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-21 12:49:03 -0500

    set up basic divs for structuring about the devs page

--- src/AboutTheDevs.jsx
+++ src/AboutTheDevs.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 
 export const AboutTheDevs = () => {
   return (
-    <div className="bg-blue-500">
-      <Link to={"/"}>Link</Link>
-      <div className="">H</div>
+    <div className="mx-2 my-2">
+      <div className="p-5 rounded-lg flex justify-between bg-yellow-500">
+        <div className="mx-2 my-2 p-5 flex-1 rounded-lg bg-blue-500">
+          <div className="border border-white"> Max Power</div>
+        </div>
+      </div>
     </div>
   );
 };
