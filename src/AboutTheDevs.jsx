c8c246e600ea87431fb5cb005d29af95f8a982a7
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-21 13:12:14 -0500

    fixed rounded corners so that sections could flow into eachother on about the devs page

--- src/AboutTheDevs.jsx
+++ src/AboutTheDevs.jsx
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 
 export const AboutTheDevs = () => {
   return (
-    <div className="mx-2">
-      <div className="p-5 rounded-lg flex justify-between bg-yellow-500">
-        <div className="mx-2 my-2 p-5 flex-1 rounded-lg bg-blue-500">
-          <div className="border border-white"> Max Power</div>
+    <>
+      <div className="mx-2">
+        <h1 className="p-10 text-3xl text-center font-semibold bg-zinc-200">
+          About the Developers
+        </h1>
+        <div className="p-5 rounded-bl flex justify-between bg-yellow-500">
+          <div className="mx-2 my-2 p-5 flex-1 rounded-lg bg-blue-500">
+            <div className="border border-white"> Max Power</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
