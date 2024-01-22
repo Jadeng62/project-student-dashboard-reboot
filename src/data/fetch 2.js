acec14ea2dc10da2ca06515a95369c41ea7b7ac6
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-22 15:07:16 -0500

    deleted extra files folder...

--- src/data/fetch 2.js
+++
@@ -1,9 +0,0 @@
-const URL = import.meta.env.VITE_BASE_API_URL;
-
-// Get All Students
-export function getAllStudents() {
-  return fetch(`${URL}`).then((response) => {
-    console.log(response); // Log the response
-    return response.json();
-  });
-}
