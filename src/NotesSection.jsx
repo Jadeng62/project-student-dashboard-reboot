e98f6e44c6beb9cf655c8a5474ffc5ecb66a13be
Carlitos <carlitosdutan@pursuit.org>
2024-01-23 12:20:39 -0500

    notes section layout complete

--- src/NotesSection.jsx
+++ src/NotesSection.jsx
@@ -12,9 +12,7 @@ export const NotesSection = () => {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // Create a new variable to store the userInput, more efficient
     const newComment = { ...userInput };
-    // Spread the comment section with the new comment
     setCommentSection([...commentSection, newComment]);
     setToggleSubmit(true);
     setUserInput({
@@ -30,49 +28,56 @@ export const NotesSection = () => {
   return (
     <div>
       <div className="h-12 bg-black">
-        <h2 className=" text-white text-xl p-2.5">1-on-1 Notes</h2>
+        <h2 className="text-white text-xl p-2.5">1-on-1 Notes</h2>
       </div>
       <form
-        className=" border-8 border-black grid grid-cols-2 p-4"
+        className="border-8 border-black grid grid-cols-2 p-4"
         onSubmit={handleSubmit}
       >
-        <div className="grid grid-rows-6 text-xl auto-cols-auto">
-          <label className="px-3" htmlFor="author">
-            Author:
-          </label>
-          <input
-            className=" border border-black px-2 py-1 mb-4 w-4/5"
-            type="text"
-            id="author"
-            name="author"
-            onChange={handleChange}
-            value={userInput.author}
-            required
-          />
-          <label className="px-3" htmlFor="comment">
-            Comment:
-          </label>
-          <input
-            className=" border border-black px-2 py-1 mb-4 w-4/5 row-span-5 align-top"
-            type="text"
-            id="comment"
-            name="comment"
-            onChange={handleChange}
-            value={userInput.comment}
-            required
-          />
-          <button
-            className="bg-black text-white px-2 py-4 w-4/5 row-span-6 mt-auto rounded hover:bg-blue-700"
-            type="submit"
-          >
-            Submit
-          </button>
+        <div className="grid grid-rows-3 gap-6 pr-10">
+          <div className="text-xl flex flex-col">
+            <label className="px-3" htmlFor="author">
+              Author:
+            </label>
+            <input
+              className="border-2 border-black px-2 py-2"
+              type="text"
+              id="author"
+              name="author"
+              onChange={handleChange}
+              value={userInput.author}
+              required
+            />
+          </div>
+          <div className="text-xl flex flex-col">
+            <label className="px-3" htmlFor="comment">
+              Comment:
+            </label>
+            <textarea
+              className="border-2 border-black px-2 resize-none"
+              id="comment"
+              name="comment"
+              onChange={handleChange}
+              value={userInput.comment}
+              rows="3"
+              required
+            />
+          </div>
+
+          <div className="pt-12">
+            <button
+              className="bg-black text-white px-4 py-2 w-full rounded hover:bg-blue-700"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
         </div>
-        <div className="border-8 border-black">
-          <h2 className="text-xl bg-black text-white h-12 p-2.5 text-center">
+        <div>
+          <h2 className="text-xl bg-black text-white p-2.5 text-center">
             Comment Section
           </h2>
-          <div className="text-lg text-center overflow-y-scroll h-80">
+          <div className="text-lg text-center overflow-y-scroll h-80 border-8 border-black">
             {toggleSubmit &&
               commentSection.map((note, index) => (
                 <div className="border-b-2 border-black p-1.5" key={index}>
